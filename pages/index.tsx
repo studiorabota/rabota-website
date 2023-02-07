5f0bb46cbb03f42490bf8f3a9932d889c1604904
Vincent Kranendonk <git@studiorabota.com>
2023-02-07 12:00:13 +0100

    Added images and link for 3d prints on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Studio Rabota | Code, education, prototyping and artistic-scientific experimentation</title>
-        <meta name="description" content="Rabota is a studio specialized in code, rapid-prototyping and artistic-scientific experimentation" />
+        <title>Studio Rabota | Coding, education, prototyping and artistic-scientific experimentation</title>
+        <meta name="description" content="Studio Rabota is a studio specialized in code, rapid-prototyping and artistic-scientific experimentation" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.intro}>Rabota | Code, prototyping, electronics and artistic-scientific experimentation 🦠</h1>
+        <h1 className={styles.intro}><span>S</span>tudio <span>R</span>abota | Coding, prototyping, electronics, open-source hardware 🪱 and some artistic-scientific experimentation 🦠</h1>
         <nav className={styles.nav}>
           <ul>
             <li><a href='#'>Wiggle Bin<span>Prototype</span></a></li>
@@ -21,6 +22,33 @@ export default function Home() {
             <li><a href='#'>Weather sensor <span>Data</span></a></li>
           </ul>
         </nav>
+        <a href='#' className={styles.cardPrints}>
+          <Image
+            src="/images/3d_gear.png"
+            width={261}
+            height={269}
+            alt="Picture of 3d printed gear"
+            className={styles.gear}
+          />
+          <Image
+            src="/images/3d_robot.png"
+            width={299}
+            height={362}
+            alt="Picture of 3d printed robot"
+            className={styles.robot}
+          />
+          <Image
+            src="/images/3d_soil_moisture_sensor_cap.png"
+            width={407}
+            height={350}
+            alt="Picture of 3d printed soil moisture sensor cap"
+            className={styles.moistureSensorCap}
+          />
+          <div className={styles.cardInfo}>
+            <h3 className={styles.cardHeader}>3D Objects</h3>
+            <span className={styles.cardDescription}>Download & Print</span>
+          </div>
+        </a>
       </main>
     </>
   )
